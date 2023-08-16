8ea972d7e5ecc8b41bdf8f94265970ee06607209
Henansh721 <88682260+Henansh721@users.noreply.github.com>
2023-08-17 03:40:05 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,4 +14,4 @@ api.add_resource(PreferenceGeneratorResource, '/generate/preference')
 api.add_resource(HomePage, '/')
   
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=8080)
