d38d1b7703711cbe469a5c3ebb515ef463a21bbc
Henansh721 <88682260+Henansh721@users.noreply.github.com>
2023-08-18 12:01:32 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,4 +11,4 @@ api = Api(app)
 api.add_resource(FashionOutfitGeneratorResource, '/generate/outfit')
   
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=8080)
