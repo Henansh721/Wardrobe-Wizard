cb36fc521f385b34f4f962840cd409384ef32e74
Henansh721 <88682260+Henansh721@users.noreply.github.com>
2023-08-20 00:43:54 +0530

    Updated.

--- src/BingImageService.py
+++ src/BingImageService.py
@@ -9,15 +9,13 @@ class BingImageSearchAPI:
     def search_images(self, _query):
         params = {"q": _query,"count": 1,"imageType": "Photo"}
 
-        response = requests.get(self.base_url, headers=self.headers, params=params)
+        response = requests.get(self.bingSearchURL, headers=self.headers, params=params)
         data = response.json()
 
-        if "value" in data:
-            images = data["value"]
+        try:
+            images = data["images"]["value"]
             if images:
                 image_info = images[0]
                 return image_info.get("thumbnailUrl")
-
-        return ""
-
-
+        except Exception as e:
+            return str(e)
