09cbbe6bcb6d3b5a7583cd7dbb4c3547c4c449e4
Henansh721 <88682260+Henansh721@users.noreply.github.com>
2023-08-19 15:32:41 +0530

    update

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ from flask_restful import Resource, Api
 from PreferenceGeneratorResource import PreferenceGeneratorResource
 from HomePage import HomePage
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(PreferenceGeneratorResource, '/generate/preference')
@@ -14,4 +16,4 @@ api.add_resource(PreferenceGeneratorResource, '/generate/preference')
 api.add_resource(HomePage, '/')
   
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0')
